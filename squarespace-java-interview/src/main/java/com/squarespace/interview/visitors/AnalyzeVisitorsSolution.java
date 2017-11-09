6272489efaa5750347235da8e5e3e6a798c8c3d7
add1droid <add1droid@users.noreply.github.com>
2017-11-08 19:08:16 -0500

    test
    test

---
+++ squarespace-java-interview/src/main/java/com/squarespace/interview/visitors/AnalyzeVisitorsSolution.java
@@ -0,0 +1,17 @@
+package com.squarespace.interview.visitors;
+
+class AnalyzeVisitorsSolution {
+
+  /**
+   * Given a {@link Iterable} of website hit data modeled as {@link WebsiteVisit} analyze it and return the
+   * resulting page views, unique visitors, and sessions.
+   *
+   * @param websiteVisits Input hit data.
+   * @return A 3-element array of long corresponding to 0: page views, 1: unique visitors, 2: view sessions
+   */
+  @SuppressWarnings("UnusedParameters")
+  static long[] processPageViews(Iterable<WebsiteVisit> websiteVisits) {
+    // @ToDo Implement this routine.
+    return new long[3];
+  }
+}
